a94e7b05f090cfe5ca01d4ba34648098b9f3b684
nanashi <you@youraddress.com>
2023-10-22 15:19:09 +0530

    finished database change

--- app.py
+++ app.py
@@ -1,40 +1,31 @@
-from flask import Flask, render_template , request, redirect , url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://default:************@ep-still-voice-77631904.us-east-1.postgres.vercel-storage.com:5432/verceldb'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://default:Iq0QrfPyXA4o@ep-still-voice-77631904.us-east-1.postgres.vercel-storage.com:5432/verceldb"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-
-
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
 
-
-
 @app.route('/')
 def index():
     todo_list = Todo.query.all()
-    return render_template("base.html",TodoList = todo_list)
+    return render_template("base.html", TodoList=todo_list)
 
-@app.route('/add',methods=["POST"])
+@app.route('/add', methods=["POST"])
 def add():
     title = request.form.get("title")
-    new_todo = Todo(title=title,complete = False)
+    new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("index"))
 
-
 @app.route('/update/<int:todo_id>')
 def update(todo_id):
-    
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
@@ -42,12 +33,12 @@ def update(todo_id):
 
 @app.route('/delete/<int:todo_id>')
 def delete(todo_id):
-    
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-        db.create_all() 
-        app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
